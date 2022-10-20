41c3ee7aa33f065f4c0743f52ecb97fd278938a0
huy22091999 <69074904+huy22091999@users.noreply.github.com>
2022-10-20 22:28:02 +0700

    huy commit

--- src/main/java/com/example/demo/api/Controler.java
+++ src/main/java/com/example/demo/api/Controler.java
@@ -6,7 +6,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controler {
     @GetMapping("/test")
-    String test() {
-        return "test";
+    User test() {
+        return new User();
+    }
+}
+class User{
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    User(){
+
     }
 }
